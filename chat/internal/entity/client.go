6546aff82b58c7ce78ca7f55a06ca5e6ce20c371
Vinicius Gabriel <contato@viniciusgabriel.com.br>
2022-08-07 20:15:52 -0300

    fix: remove uselesss code

--- chat/internal/entity/client.go
+++ chat/internal/entity/client.go
@@ -23,32 +23,3 @@ func NewClient(wsConn *websocket.Conn, user *User, chatroom *Chatroom) *Client {
 func (c *Client) GetOutboundChan() chan *Message {
 	return c.outbound
 }
-
-// func (c *Client) Read() {
-// 	defer c.websocket.Close()
-
-// 	for {
-// 		_, bMsg, err := c.websocket.ReadMessage()
-// 		if err != nil {
-// 			if !websocket.IsCloseError(err, websocket.CloseNormalClosure, websocket.CloseGoingAway) {
-// 				// TODO: handle this error correctly
-// 				log.Println(err)
-// 			}
-// 			break
-// 		}
-
-// 		c.chatRoom.broadcast <- NewMesssage(uuid.UUID{}, bMsg)
-// 	}
-// }
-
-// func (c *Client) Write() {
-// 	defer c.websocket.Close()
-
-// 	for msg := range c.outbound {
-// 		if err := c.websocket.WriteJSON(msg); err != nil {
-// 			// TODO: handle this error correctly
-// 			log.Println(err)
-// 			break
-// 		}
-// 	}
-// }
